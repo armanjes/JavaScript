b4305c3616de34ced7307c0fbc76627b90dc033e
Md. Arman Hossen <mdarmanjes@gmail.com>
2023-09-18 20:38:26 +0600

    html element selector

--- Query Selector/index.js
+++ Query Selector/index.js
@@ -1,2 +1,8 @@
+
+
+
 document.querySelectorAll("p")[0].innerHTML = "this is text 1";
+
 document.querySelectorAll("p")[1].innerHTML = "this is text 2";
+
+
---
+++ select html elements/index.js
@@ -0,0 +1,16 @@
+
+// getElementById
+
+/*var change = document.getElementById("heading2");
+
+change.innerHTML = "good bye. good bye."*/
+
+//--------------------------------------------------------------------
+
+//getElementsByTagName
+
+var change = document.getElementsByTagName("h1")[0];
+change.innerHTML = "hi";
+
+var change = document.getElementsByTagName("h1")[1];
+change.innerHTML = "hi 1";
