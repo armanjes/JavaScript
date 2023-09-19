e296680ddfffc3f8a21bfc9ff4b44e132a547969
Md. Arman Hossen <mdarmanjes@gmail.com>
2023-09-19 20:35:02 +0600

    new updated

--- Multi Event Listener/index.js
+++ Multi Event Listener/index.js
@@ -1,3 +1,15 @@
 
-var heading = document.getElementById("h2");
 
+var clickButton = document.querySelectorAll(".button");
+var heading = document.querySelector("h1");
+
+for(var i = 0; i <= clickButton.length; i++){
+    clickButton[i].addEventListener(
+        "click",
+        function(){
+            var text = this.innerHTML;
+            heading.innerHTML = text + " clicked.";
+        }
+    
+    )
+}
