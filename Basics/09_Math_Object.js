c4659adb3b1a026d2c6c1784fa508850363b99de
Arman <mdarmanjes@gmail.com>
2024-05-02 19:36:29 +0600

    Math object

---
+++ Basics/09_Math_Object.js
@@ -0,0 +1,35 @@
+// ✅ math object
+
+let x;
+
+//🚀 Square root
+x = Math.sqrt(9);
+
+//🚀 Absolute value
+x = Math.abs(-5); // converts negative number to positive
+
+//🚀 Round
+x = Math.round(4.2); // if decimal value goes upto .4 will give 5 else 4
+
+//🚀 Round up
+x = Math.ceil(4.2); // 5
+
+//🚀 Round down
+x = Math.floor(4.9); // 4
+
+//🚀 Exponent
+x = Math.pow(2, 3); // 8
+
+//🚀 Minimum number
+x = Math.min(4, 5, 3); // 3 
+
+//🚀 Maximum number
+x = Math.max(4, 5, 3); // 5
+
+//🚀 Get a random number/decimal between 0 and 1
+x = Math.random();
+
+//🚀 Get a random number between 1 and 100
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
